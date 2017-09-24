6ca3f1129ceb2f170ac0799188213c2430946991
Nurul Huda <hudasanca@gmail.com>
2017-09-24 14:05:53 +0700

    first-program (oop style)

---
+++ 01. first-program-oop.py
@@ -0,0 +1,24 @@
+import sys
+from PySide import QtGui
+
+"""
+Jago Ngoding tutorial PySide berbasa Indonesia
+
+author: Ibnu Jakaria
+"""
+
+class Jendela(QtGui.QWidget):
+
+  def __init__(self):
+    super(Jendela, self).__init__()
+    
+    self.siapkanUI()
+
+  def siapkanUI(self):
+    self.resize(800, 600)
+    self.setWindowTitle('Jago Ngoding')
+
+app = QtGui.QApplication(sys.argv)
+jendela = Jendela()
+jendela.show()
+sys.exit(app.exec_())
